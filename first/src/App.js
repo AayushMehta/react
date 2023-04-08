132438763d99f2378b7e253a88e27b1b1c153d65
AayushMehta <aayushmehta5225@gmail.com>
2023-04-08 18:11:00 +0530

    conponents

--- first/src/App.js
+++ first/src/App.js
@@ -1,5 +1,8 @@
-import ExpenseItem from "./components/ExpenseItem";
-function App() {
+import React from "react";
+
+import Expenses from "./components/Expenses";
+
+const App = () => {
   const expenses = [
     {
       id: "e1",
@@ -21,31 +24,20 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
+
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', {}, "Let's get started!"),
+  //   React.createElement(Expenses, { items: expenses })
+  // );
+
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem>
-        amount={expenses[0].amount}
-        title={expenses[0].title}
-        date={expenses[0].date}
-      </ExpenseItem>
-      <ExpenseItem>
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      </ExpenseItem>
-      <ExpenseItem>
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      </ExpenseItem>
-      <ExpenseItem>
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      </ExpenseItem>
+      <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
